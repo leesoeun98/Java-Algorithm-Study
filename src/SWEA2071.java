7f1fb10fbb3fcdb3e230f7f66f7b416d5bb6031c
soeun lee <thdmsdl87@gmail.com>
2021-01-21 01:46:26 +0900

    2021.01.21 - swea 2문제

---
+++ src/SWEA2071.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SWEA2071 {
+    static public void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < 10; j++) {
+                int num = scan.nextInt();
+                sum+=num;
+            }
+            System.out.println("#"+(i+1)+" "+Math.round(sum/10.0));//자료형주의
+        }
+    }
+}
---
+++ src/SWEA2072.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SWEA2072 {
+    static public void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < 10; j++) {
+                int num = scan.nextInt();
+                if (num % 2 == 1) {
+                    sum += num;
+                }
+            }
+            System.out.println("#"+(i+1)+" "+sum);
+        }
+    }
+}
