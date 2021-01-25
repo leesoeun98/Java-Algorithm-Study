19706c340a3dacfcc4eb031564ab70cc5a7109ac
soeun lee <thdmsdl87@gmail.com>
2021-01-25 12:31:09 +0900

    백준 14502, 14888 수정

--- src/baekjoon14502.java
+++ src/baekjoon14502.java
@@ -1,11 +1,24 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class baekjoon14502 {
+    static class VirusDot {
+        int x;
+        int y;
+
+        public VirusDot(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     static int n;
     static int m;
     static int center[][];
+    static int copiedmap[][];
     static ArrayList<Integer> safezone;
+    static ArrayList<VirusDot> virusList = new ArrayList<VirusDot>();
+    static int x[] = {-1, 1, 0, 0};
+    static int y[] = {0, 0, -1, 1};
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -16,25 +29,70 @@ public class baekjoon14502 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 center[i][j] = scan.nextInt();
+                if (center[i][j] == 2) {
+                    virusList.add(new VirusDot(i, j));
+                }
             }
         }
-        //wall 호출
-        //max 출력
+        wall(0, 0);
+        System.out.println(Collections.max(safezone));
 
     }
 
-    static void wall(int wall_count) {
+    static void wall(int wall_count, int start) {
         if (wall_count == 3) {
+            //새로운 맵에 바이러스 퍼뜨리기 (기존 연구소에 영향 안가게)
+            copiedmap = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    copiedmap[i][j] = center[i][j];
+                }
+            }
             //바이러스 퍼뜨리기
-            //안전 영역 크기 구해서 저장
-            //safezone.add();
+            for (VirusDot virus : virusList) {
+                spreadVirus(virus.x, virus.y);
+            }
+
+            safezone.add(safeCount());
             return;
         }
-        //n*m에서 반복 {
-        //0이 있으면 1로 바꾸기
-        //다음 wall 함수 호출
-        //바꾼 1을 다시 0으로 복원 }
+        for (int i = start; i < n * m; i++) {
+            //x행 y열 
+            int x = i / m;
+            int y = i % m;
+            if (center[x][y] == 0) {
+                center[x][y] = 1;
+                //depth 3번될때까지
+                wall(wall_count + 1, i + 1);
+                //한 번 safezone계산 완료 후에 다시 계산하기 위해 center원상 복귀
+                center[x][y] = 0;
+            }
+        }
+    }
 
+    static void spreadVirus(int i, int j) {
+        for (int direction = 0; direction < 4; direction++) {
+            int nearx = i + x[direction];
+            int neary = j + y[direction];
+            if (0 <= nearx && nearx < n && 0 <= neary && neary < m) {
+                if (copiedmap[nearx][neary] == 0) {
+                    copiedmap[nearx][neary] = 2;
+                    spreadVirus(nearx, neary);
+                }
+            }
+        }
+    }
+
+    static int safeCount() {
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (copiedmap[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
 }
--- src/baekjoon14888.java
+++ src/baekjoon14888.java
@@ -1,59 +1,68 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+//baekjoon14888
 public class baekjoon14888 {
 	static int n;
 	static ArrayList<Integer> arr;
 	static int[] nums;
 	static int[] operations;
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		n=scan.nextInt();
+		n = scan.nextInt();
 		arr = new ArrayList<Integer>();
-		
+
 		nums = new int[n];
-		
+
 		operations = new int[4];
-		
-		for(int i=0; i<n; i++) {
-			nums[i]=scan.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			nums[i] = scan.nextInt();
 		}
-		for(int i=0; i<4; i++) {
-			operations[i]=scan.nextInt();
+		for (int i = 0; i < 4; i++) {
+			operations[i] = scan.nextInt();
 		}
-		//모든 경우의 수를 다 탐색하기 위헤 dfs
-		// 계산결과인 result와 number의 idx가 인자 
-		dfs(nums[0],1);
+		// 모든 경우의 수를 다 탐색하기 위헤 dfs
+		// 계산결과인 result와 number의 idx가 인자
+		dfs(nums[0], 1);
+
 		System.out.println(Collections.max(arr));
 		System.out.println(Collections.min(arr));
-		
+
 	}
-	
+
 	static void dfs(int result, int nidx) {
-		//종료 조건 => number 다 썼으면 종료 (최종 계산결과를 arraylist에 저장)
-		if(nidx==n) {
+		// 종료 조건 => number 다 썼으면 종료 (최종 계산결과를 arraylist에 저장)
+		if (nidx == n) {
 			arr.add(result);
 			return;
 		}
-		for(int i=0; i<4; i++) {
-			if(operations[i]>0) {
+		for (int i = 0; i < 4; i++) {
+			if (operations[i] > 0) {
 				operations[i]--;
-				switch(i) {
+				switch (i) {
 				case 0:
-					dfs(result+nums[nidx], nidx+1);
+					dfs(result + nums[nidx], nidx + 1);
+					System.out.printf("%d %d %n", i, nidx);
 					break;
 				case 1:
-					dfs(result-nums[nidx], nidx+1);
+					dfs(result - nums[nidx], nidx + 1);
+					System.out.printf("%d %d %n", i, nidx);
 					break;
 				case 2:
-					dfs(result*nums[nidx], nidx+1);
+					dfs(result * nums[nidx], nidx + 1);
+					System.out.printf("%d %d %n", i, nidx);
 					break;
 				case 3:
-					dfs(result/nums[nidx], nidx+1);
+					dfs(result / nums[nidx], nidx + 1);
+					System.out.printf("%d %d %n", i, nidx);
 					break;
 				}
 				operations[i]++;
 			}
+			System.out.println();
 		}
 	}
 
