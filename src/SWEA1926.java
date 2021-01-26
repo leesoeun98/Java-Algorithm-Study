6cc48c9ed04fdfeec20c82b6c598f8b57052a526
soeun lee <thdmsdl87@gmail.com>
2021-01-27 08:42:13 +0900

    2021.01.27 SWEA D1~D2

--- src/SWEA1926.java
+++ src/SWEA1926.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class SWEA1926 {
+import java.util.Scanner;
+
+public class SWEA1926 {
+    static public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if(game(i)>0){
+                for (int j = 0; j < game(i); j++) {
+                    System.out.print("-");
+                }
+                System.out.print(" ");
+            }else{
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+    static int game(int i) {
+        int count = 0;
+        while (i > 0) {
+            if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+                count++;
+            }
+            i /= 10;
+        }
+        return count;
+    }
 }
---
+++ src/SWEA2007.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class baekjoon2007 {
+    static public void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 0; i < num; i++) {
+            String word = sc.next();
+
+            for (int j = 1; j <= 10; j++) {
+                //문자열이면 substring 쓰기
+                if (word.substring(0, j).equals(word.substring(j, 2 * j))) {
+                    System.out.println("#" + i + " " + j);
+                    break;
+                }
+            }
+        }
+    }
+}
