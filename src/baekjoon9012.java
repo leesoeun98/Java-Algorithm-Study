5dd004164cb04a1614ec9f38639c346611b1582c
soeun lee <thdmsdl87@gmail.com>
2021-03-08 17:43:03 +0900

    2021.03.08 백준 - 2문제

--- src/baekjoon9012.java
+++ src/baekjoon9012.java
@@ -3,42 +3,43 @@ import java.util.Stack;
 
 public class baekjoon9012 {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int count = scan.nextInt();
-		scan.nextLine();
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int count = scan.nextInt();
+        scan.nextLine();
 
-		for (int i = 0; i < count; i++) {
-			// 스택 반복문 실마다 초기화되도록 여기에 선언 필요
-			Stack<Character> stack = new Stack<Character>();
-			String pstring = scan.nextLine();
-			int isVPS = 0;
-			int j = 0;
+        for (int i = 0; i < count; i++) {
+            String ps = scan.next();
+            Stack pstack = new Stack();
+			boolean answer = true;
+
 			/*
-			 * 각 괄호마다 ( 면 push ) 인데 스택이 비워져있으면 no ) 인데 스택에 뭐가 있으면 pop => 모든 괄호에 대한 판단 이후에
-			 * stack이 안비어져있으면 no 비어져있으면 yes
-			 */
-			while (j < pstring.length()) {
-				if (pstring.charAt(j) == '(') {
-					stack.push('(');
-				} else if (pstring.charAt(j) == ')' && !stack.empty()) {
-					stack.pop();
-				} else if (pstring.charAt(j) == ')' && stack.empty()) {
-					isVPS = 1;
-					break;
-				}
-				j++;
-			}
-			if (!stack.empty()) {
-				isVPS = 1;
-			}
-			if (isVPS == 0) {
-				System.out.println("YES");
-			} else {
-				System.out.println("NO");
-			}
-		}
+			 *  ( 면 stack에 넣기
+			 *  ) 인데 비어져있으면 no
+			 *    인데 ( 있으면 빼기
+			 *  입력 종료 됐는데 스택에 뭐가 남아있으면 no
+			 *
+			 * */
 
-	}
+            for (int j = 0; j < ps.length(); j++) {
+                if (ps.charAt(j) == '(') {
+                    pstack.add(ps.charAt(j));
+                } else {
+                    if (pstack.empty()) {
+                    	answer = false;
+                    	break;
+                    }else{
+                    	pstack.pop();
+					}
+                }
+            }
+            if(!pstack.empty()){
+            	answer = false;
+			}
+            if(answer) System.out.println("YES");
+            else System.out.println("NO");
+
+        }
+    }
 
 }
